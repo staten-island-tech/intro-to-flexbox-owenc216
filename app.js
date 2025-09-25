077f644a63081d06cb746c4dd18a647fcfb3bcf7
owenc216 <owen216413@gmail.com>
2025-09-25 13:12:00 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+// we declare and define variables
+/* let x = 5;
+x = 6; */
+/* const x = 5;
+x = 6;
+console.log(x);
+ */
+// strings represent names,characters letters etc
+/* const x = "Ian";
+
+//intergers/numbers
+const y = 12345;
+
+//boolean or true/false
+const citizen = true;
+
+//null is the absence of value
+let name = null;
+
+//undefined 
+let z;
+ */
+//functions are input/output machines
+//functions accept inputs or parameters
+//declaring an defining functions
+/* function add(x, y) {
+  //console.log(x + y)
+  console.log(x + y);
+  return x + y;
+}
+//run the function
+const bill = add(9, 8);
+console.log(bill); */
+/* const x = 7;
+
+function returnThing() {
+  const x = 8;
+  console.log(x);
+}
+returnThing();
+//template literals
+const name = "nelson";
+console.log("fsf ${name}");
+
+console.log(5 % 2);
+ */
+function rocket(P, B, D) {
+    const bages = Math.floor(P/B);
+    const leftover = P%B;
+    const money = ;badges *D + leftover;
+    console.log(money);
+}
