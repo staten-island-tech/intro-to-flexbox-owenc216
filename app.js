d267595d874cf3612b03af21d0563952f87c3b52
owenc216 <owen216413@gmail.com>
2025-09-25 13:53:00 -0400

    lebron

--- app.js
+++ app.js
@@ -44,9 +44,44 @@ console.log("fsf ${name}");
 
 console.log(5 % 2);
  */
-function rocket(P, B, D) {
+/* function rocket(P, B, D) {
     const bages = Math.floor(P/B);
     const leftover = P%B;
     const money = ;badges *D + leftover;
     console.log(money);
 }
+ */
+/* const age = 18
+if (age >= 18){
+  console.log("vote");
+} else {
+  console.log("no vote")
+}
+
+const temp = 32
+if (temp >= 212){
+  console.log("boil");
+} else if (temp <= 32) {
+  console.log("frozen")
+} else {
+  console.log("water")
+} */
+// 3= compare and evaluate, 2= evaluate and ignore
+// or is ||
+/*   const age = 35;
+  const citizen = true;
+
+  if 1 == "1"
+
+  if (age === true && citizen === true) {
+    console.log("can run for pres");
+  } */
+/*  function enterCastle(hasKey, hasMagic, isBanned){
+  if (hasKey === true || hasMagic  && !isBanned) {
+    console.log("can enter castle");
+  } else {
+    console.log("cannot enter");
+  }
+ }
+ enterCastle(true, true, true); */
+function 
