aba96e1fca5a464b5f88435684dcd4cfd6e02677
owenc216 <owen216413@gmail.com>
2025-10-08 14:02:07 -0400

    que

--- index.js
+++ index.js
@@ -1,17 +1,153 @@
-const books = [
+const Rice = [
   {
-    name: "Basmati",
+    name: "Sushi Rice",
     price: 8,
+    img: "https://th.bing.com/th/id/OIP.9JJ6ukwiQRrY2QUdVZzF-AHaHa?w=220&h=220&c=7&r=0&o=7&pid=1.7&rm=3",
+    type: "Short-Grain",
+    origin: "Japan",
+  },
+  {
+    name: "Glutinous Rice",
+    price: 8,
+    img: "https://unitedasianfoods.com/wp-content/uploads/2024/11/RR003-4x6-1.jpg",
+    type: "Short-Grain",
+    origin: "Southeast Asia",
+  },
+  {
+    name: "Calrose Rice",
+    price: 8,
+    img: "https://i5.walmartimages.com/seo/Homai-Calrose-Medium-Grain-Fancy-Rice-50-lb_90e86c8f-58fd-4dd0-9942-0328e2d1cf9c.574cc9fdc87e98281463ea17ddb33671.jpeg?odnHeight=580&odnWidth=580&odnBg=FFFFFF",
+    type: "Short-Grain",
+    origin: "USA, California",
+  },
+  {
+    name: "Bomba Rice",
+    price: 8,
+    img: "https://savorygourmet.com/cdn/shop/files/SANTO_TOMAS_Rice_Bomba_1_kg_10665_5defddd6-94fc-4f90-84e9-5599da085ff2_2000x2000.png?v=1703161804",
+    type: "Short-Grain",
+    origin: "Spain",
+  },
+  {
+    name: "Arborio Rice",
+    price: 8,
+    img: "https://th.bing.com/th/id/OIP.ffiKzcqLU4vDUYSg4rtVTAHaHa?w=211&h=212&c=7&r=0&o=7&pid=1.7&rm=3",
+    type: "Short-Grain",
+    origin: "Italy",
+  },
+  {
+    name: "Valencia Rice",
+    price: 8,
+    img: "https://scene7.samsclub.com/is/image/samsclub/0008562501202_A?$DT_PDP_BB$",
+    type: "Short-Grain",
+    origin: "Spain",
+  },
+  {
+    name: "Jeju Rice",
+    price: 8,
+    img: "https://img06.weeecdn.com/item/image/892/441/2283EBF16866B103.png",
+    type: "Short-Grain",
+    origin: "Korea",
+  },
+  {
+    name: "Carnaroli Rice",
+    price: 8,
+    img: "https://tse1.mm.bing.net/th/id/OIP._wyRnq22Q6eSyepAOpojygHaHa?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
+    type: "Medium-Grain",
+    origin: "Italy",
+  },
+  {
+    name: "Baldo Rice",
+    price: 8,
+    img: "https://qualitamart.com/cdn/shop/files/DuruBaldoRiceRemake_590X650_crop_center.jpg?v=1689977561",
+    type: "Medium-Grain",
+    origin: "Italy",
+  },
+  {
+    name: "Calasparra Rice",
+    price: 8,
+    img: "https://ibericotaste.us/cdn/shop/files/calasparra-rice_5ca5ad1a-d41e-4781-9f7c-47795953d2e9.jpg?v=1713121028&width=1200",
+    type: "Medium-Grain",
+    origin: "Spain",
+  },
+  {
+    name: "SunRice Rice",
+    price: 8,
+    img: "https://th.bing.com/th/id/R.7aec082d3f145b1c5bcba324bc1fc1ac?rik=A0pJRSMAGvUBuA&pid=ImgRaw&r=0",
+    type: "Medium-Grain",
+    origin: "Australia",
+  },
+  {
+    name: "Sona Masoori Rice",
+    price: 8,
+    img: "https://m.media-amazon.com/images/I/81hM8AxwHyL._AC_.jpg",
+    type: "Medium-Grain",
+    origin: "India",
+  },
+  {
+    name: "Egyptian Rice",
+    price: 8,
+    img: "https://bf1af2.a-cdn.akinoncloud.com/products/2024/09/12/71734/9218e02c-be26-4cb8-b870-bda6854445b1_size3840_cropCenter.jpg",
+    type: "Medium-Grain",
+    origin: "Egypt",
+  },
+  {
+    name: "Basmati Rice",
+    price: 8,
+    img: "https://cdnimg.webstaurantstore.com/images/products/xxl/235904/1978743.jpg",
+    type: "Long-Grain",
+    origin: "India",
+  },
+  {
+    name: "Jasmine Rice",
+    price: 8,
+    img: "https://th.bing.com/th/id/OIP.lKdPOv79_ZQrWQ9EKJWtkwHaHa?w=196&h=196&c=7&r=0&o=7&pid=1.7&rm=3",
+    type: "Long-Grain",
+    origin: "Thailand",
+  },
+  {
+    name: "American Rice",
+    price: 8,
+    img: "https://i5.walmartimages.com/seo/Great-Value-Long-Grain-Enriched-Rice-20-lb_c2f6d109-09d5-4b78-a2c7-80da793c9ad5.5f820eb042b2a573b4e2cd0fa73c9852.jpeg",
+    type: "Long-Grain",
+    origin: "USA",
+  },
+  {
+    name: "Texmati Rice",
+    price: 8,
+    img: "https://m.media-amazon.com/images/I/61CV4BXplTL._SL1000_.jpg",
+    type: "Long-Grain",
+    origin: "USA",
+  },
+  {
+    name: "Wehani Rice",
+    price: 8,
+    img: "https://m.media-amazon.com/images/I/810GUPjs4EL._SL1500_.jpg",
+    type: "Long-Grain",
+    origin: "USA",
+  },
+  {
+    name: "Red Rice",
+    price: 8,
+    img: "https://images.deliveryhero.io/image/darkstores/groceries-catalog/products-uae/UAE_8857109700254.jpg",
+    type: "Long-Grain",
+    origin: "Bhutan",
+  },
+  {
+    name: "Wild Rice",
+    price: 8,
+    img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ_6AEI69bGB8R8bTLFtauZ6vmIRqI1LW43QG08d5dmncFP6LRnoPimT-MMDgZoJPnZZAJNmqaM_E0V_CmI4Sy4SmMTDeU74jdYMhfPj5WSFBUEiCYhbzYj8aAc2PYkLB6AvrHasQ&usqp=CAc",
+    type: "Long-Grain",
+    origin: "North America",
   },
 ];
-function inject(album) {
+function inject(Rice) {
   DOMSelectors.display.insertAdjacentHTML(
     "afterbegin",
-    `<div class="display-card">
-      <img class="display-img" src="${album.url}"/>
-      <h2 class="display-artist">${album.artist}</h2>
-      <h3 class="display-album">${album.title}</h3>
+    <div class="display-card">
+      <img class="display-img" src="${album.url}" />
+      <h2 class="display-artist">${album.name}</h2>
+      <h3 class="display-album">${album.price}</h3>
       <button class="remove btn">Remove Album</button>
-    </div>`
+    </div>
   );
 }
