2d5eebc64da2482cfde96ee00c562ce8114f81b5
owenc216 <owen216413@gmail.com>
2025-10-24 12:39:16 -0400

    ochen da

--- index.js
+++ index.js
@@ -1,147 +1,147 @@
 const Rice = [
   {
     name: "Sushi Rice",
-    price: 100,
+    price: 445.99,
     img: "https://th.bing.com/th/id/OIP.9JJ6ukwiQRrY2QUdVZzF-AHaHa?w=220&h=220&c=7&r=0&o=7&pid=1.7&rm=3",
     type: "Short-Grain",
     origin: "Japan",
   },
   {
     name: "Glutinous Rice",
-    price: 100,
+    price: 539.99,
     img: "https://unitedasianfoods.com/wp-content/uploads/2024/11/RR003-4x6-1.jpg",
     type: "Short-Grain",
     origin: "Southeast Asia",
   },
   {
     name: "Calrose Rice",
-    price: 100,
+    price: 589.99,
     img: "https://thvnext.bing.com/th/id/OIP.MqeTaX3SdOu2onvvk8bAgAHaKA?w=130&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Short-Grain",
     origin: "USA, California",
   },
   {
     name: "Bomba Rice",
-    price: 100,
+    price: 364.99,
     img: "https://thvnext.bing.com/th/id/OIP.XDHYY2TA_oo5wc_p7croKwHaHa?w=177&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Short-Grain",
     origin: "Spain",
   },
   {
     name: "Arborio Rice",
-    price: 100,
+    price: 565.99,
     img: "https://th.bing.com/th/id/OIP.ffiKzcqLU4vDUYSg4rtVTAHaHa?w=211&h=212&c=7&r=0&o=7&pid=1.7&rm=3",
     type: "Short-Grain",
     origin: "Italy",
   },
   {
     name: "Valencia Rice",
-    price: 100,
+    price: 115.99,
     img: "https://thvnext.bing.com/th/id/OIP.qExTjEyoVn9OrZRmvSssswHaHa?w=213&h=213&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Short-Grain",
     origin: "Spain",
   },
   {
     name: "Jeju Rice",
-    price: 100,
+    price: 820.99,
     img: "https://thvnext.bing.com/th/id/OIP.5Joswp9qOkW9sDYun85AmwHaHa?w=175&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Short-Grain",
     origin: "Korea",
   },
   {
     name: "Carnaroli Rice",
-    price: 100,
+    price: 623.99,
     img: "https://tse1.mm.bing.net/th/id/OIP._wyRnq22Q6eSyepAOpojygHaHa?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
     type: "Medium-Grain",
     origin: "Italy",
   },
   {
     name: "Baldo Rice",
-    price: 100,
+    price: 716.99,
     img: "https://qualitamart.com/cdn/shop/files/DuruBaldoRiceRemake_590X650_crop_center.jpg?v=161009977561",
     type: "Medium-Grain",
     origin: "Italy",
   },
   {
     name: "Calasparra Rice",
-    price: 100,
+    price: 993.99,
     img: "https://thvnext.bing.com/th/id/OIP.ES_r_7pwlQoHBerCanGJVgHaJQ?w=136&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Medium-Grain",
     origin: "Spain",
   },
   {
     name: "Black Rice",
-    price: 100,
+    price: 157.99,
     img: "https://thvnext.bing.com/th/id/OIP.OkSyiwEacp_2LnFBY-fhzgHaHa?w=210&h=210&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Medium-Grain",
     origin: "China",
   },
   {
     name: "SunRice Rice",
-    price: 100,
+    price: 827.99,
     img: "https://bkk.com.au/wp-content/uploads/2022/07/Sunwhite-rice-10kg-Medium-GrainSpecial.jpg",
     type: "Medium-Grain",
     origin: "Australia",
   },
   {
     name: "Sona Masoori Rice",
-    price: 100,
+    price: 162.99,
     img: "https://www.kesargrocery.com/images/P/Royal%20Organic%20Sona%20Masoori%20Rice%20-%2020%20LB.webp",
     type: "Medium-Grain",
     origin: "India",
   },
   {
     name: "Egyptian Rice",
-    price: 100,
+    price: 442.99,
     img: "https://d2j6dbq0eux0bg.cloudfront.net/images/16880498/4261832090.png",
     type: "Medium-Grain",
     origin: "Egypt",
   },
   {
     name: "Basmati Rice",
-    price: 100,
+    price: 452.99,
     img: "https://www.indusbasket.com/cdn/shop/products/grains-basmati-rice-20-lb-bag-royal-20082086379686_1024x.jpg?v=1602860759",
     type: "Long-Grain",
     origin: "India",
   },
   {
     name: "Jasmine Rice",
-    price: 100,
+    price: 710.99,
     img: "https://th.bing.com/th/id/OIP.lKdPOv79_ZQrWQ9EKJWtkwHaHa?w=196&h=196&c=7&r=0&o=7&pid=1.7&rm=3",
     type: "Long-Grain",
     origin: "Thailand",
   },
   {
     name: "American Rice",
-    price: 100,
+    price: 285.99,
     img: "https://i5.walmartimages.com/seo/4-pack-Riceland-Enriched-Long-Grain-White-Rice-20lb_ac3b45a9-b96a-4c62-be6d-e729695e4e28.ff9affe3eaa50766c42712caf8f73f29.jpeg",
     type: "Long-Grain",
     origin: "USA",
   },
   {
     name: "Texmati Rice",
-    price: 100,
+    price: 214.99,
     img: "https://m.media-amazon.com/images/I/61CV4BXplTL._SL1000_.jpg",
     type: "Long-Grain",
     origin: "USA",
   },
   {
     name: "Wehani Rice",
-    price: 100,
+    price: 100.99,
     img: "https://thvnext.bing.com/th/id/OIP.IXJIl8fm3tFIjghTP1MJCwHaJZ?w=145&h=185&c=7&r=0&o=7&cb=12&pid=1.7&rm=3&ucfimg=1",
     type: "Long-Grain",
     origin: "USA",
   },
   {
     name: "Red Rice",
-    price: 100,
+    price: 983.99,
     img: "https://tse1.mm.bing.net/th/id/OIP.nb6Be5b69iWhLO92O0g3GQHaHa?cb=12ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
     type: "Long-Grain",
     origin: "Bhutan",
   },
   {
     name: "Wild Rice",
-    price: 100,
+    price: 750.99,
     img: "https://m.media-amazon.com/images/I/61I4R6ekpmL._AC_SX522_.jpg",
     type: "Long-Grain",
     origin: "North America",
@@ -183,13 +183,13 @@ let total = 0;
 
 function addToCart() {
   const buttons = document.querySelectorAll(".btn");
-  buttons.forEach ((button) => {
+  buttons.forEach((button) => {
     button.addEventListener("click", function () {
       const card = button.parentElement;
       const name = card.querySelector(".card-header").textContent;
       const priceText = card.querySelector(".card-price").textContent;
       const price = Number(priceText.replace("$", ""));
-      cart.push({ name: name, price: price});
+      cart.push({ name: name, price: price });
       total += price;
       showCart();
     });
@@ -199,10 +199,10 @@ function showCart() {
   const cartItems = document.querySelector(".cartItems");
   const cartTotal = document.querySelector(".cartTotal");
   cartItems.innerHTML = "";
-  cart.forEach((item) =>{
+  cart.forEach((item) => {
     cartItems.insertAdjacentHTML(
       "beforeend",
-      `<p>${item.name} - $${item.price}</p>`
+      `<h4>${item.name} - $${item.price}</h4>`
     );
   });
   cartTotal.textContent = "Total: $" + total;
